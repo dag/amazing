06191eee5893a5bc922aa7c59af43a0232d6d349
Dag Odenhall <dag.odenhall@gmail.com>
2008-04-09 11:35:43 +0200

    CPUInfo widget

---
+++ lib/amazing/widgets/cpuinfo.rb
@@ -0,0 +1,24 @@
+# Copyright (C) 2008 Dag Odenhall <dag.odenhall@gmail.com>
+# Licensed under the Academic Free License version 3.0
+
+require 'amazing/widget'
+require 'amazing/proc_file'
+
+module Amazing
+  module Widgets
+
+    # TODO: There might be more yummy stuff in /proc/cpuinfo
+    class CPUInfo < Widget
+      description "CPU Information"
+      option :cpu, "CPU number for default format (0 based)", 0
+      field :speed, "CPU Speed in MHz", []
+      default { @speed[@cpu].to_i  }
+
+      init do
+        ProcFile.parse_file("cpuinfo").each_with_index do |info, cpu|
+          @speed[cpu] = info["cpu MHz"].to_f
+        end
+      end
+    end
+  end
+end

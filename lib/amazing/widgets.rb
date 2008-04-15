01a013fd7022388b3cb5209ab6678d03559aaf38
Dag Odenhall <dag.odenhall@gmail.com>
2008-04-15 06:58:06 +0200

    Rename widget files to use proper snake case

--- lib/amazing/widgets.rb
+++ lib/amazing/widgets.rb
@@ -1,20 +1,20 @@
 # Copyright (C) 2008 Dag Odenhall <dag.odenhall@gmail.com>
 # Licensed under the Academic Free License version 3.0
 
-require 'amazing/widgets/acadapter'
+require 'amazing/widgets/ac_adapter'
 require 'amazing/widgets/alsa'
 require 'amazing/widgets/battery'
 require 'amazing/widgets/clock'
-require 'amazing/widgets/cpuinfo'
-require 'amazing/widgets/cpuusage'
+require 'amazing/widgets/cpu_info'
+require 'amazing/widgets/cpu_usage'
 require 'amazing/widgets/file'
-require 'amazing/widgets/filesystem'
+require 'amazing/widgets/file_system'
 require 'amazing/widgets/gmail'
 require 'amazing/widgets/maildir'
 require 'amazing/widgets/memory'
 require 'amazing/widgets/moc'
 require 'amazing/widgets/mpd'
-require 'amazing/widgets/nettraffic'
+require 'amazing/widgets/net_traffic'
 require 'amazing/widgets/pacman'
 require 'amazing/widgets/raggle'
 

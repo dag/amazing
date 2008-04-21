8b0162cba26dc0e3821cebc93858bdfe52bb72d0
Dag Odenhall <dag.odenhall@gmail.com>
2008-04-21 08:56:11 +0200

    Sort requires everywhere

--- lib/amazing.rb
+++ lib/amazing.rb
@@ -1,11 +1,11 @@
 # Copyright (C) 2008 Dag Odenhall <dag.odenhall@gmail.com>
 # Licensed under the Academic Free License version 3.0
 
-require 'amazing/x11'
 require 'amazing/awesome'
+require 'amazing/cli'
+require 'amazing/options'
 require 'amazing/proc_file'
+require 'amazing/string'
 require 'amazing/widget'
 require 'amazing/widgets'
-require 'amazing/string'
-require 'amazing/options'
-require 'amazing/cli'
+require 'amazing/x11'

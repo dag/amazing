b69bc6cd1936c9a45d6057eb19940519e7964019
Dag Odenhall <dag.odenhall@gmail.com>
2008-05-04 05:01:31 +0200

    Fix tiny issue with camel_case

--- lib/amazing/string.rb
+++ lib/amazing/string.rb
@@ -14,6 +14,6 @@
 
 class String
   def camel_case
-    split(/[\s_-]/).map {|t| t[0].chr.upcase + t[1..-1] }.join
+    split(/[\s_-]/).map {|t| t[0].chr.upcase + t[1..-1] unless t.empty? }.join
   end
 end
